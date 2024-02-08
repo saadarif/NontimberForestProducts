5a474eef7c007a7028ed76d7f9826beff52f8c16
Saad Arif <arifsaad@gmail.com>
2024-02-08 10:15:28 +0000

    adding images for taxa

--- app.R
+++ app.R
@@ -5,6 +5,23 @@ library(magrittr)
 library(ggplot2)
 library(vroom) #data input
 library(shiny)
+library(png)
+library(patchwork)
+
+
+#### Helper functions
+
+pickImage <- function (imageName) {
+  #uses the animal input column to pick the appropriate image to display
+  img = switch(
+    imageName,
+    "Spider" = readPNG("./images/spider.png", native = TRUE),
+    "Birds" = readPNG("./images/bird.png", native = TRUE),
+    "Insect" = readPNG("./images/insect.png", native = TRUE),
+    "Ants" = readPNG("./images/ant.png", native = TRUE),
+    "Tree" = readPNG("./images/tree.png", native = TRUE)
+  )
+}
 
 
 #Data tidying
@@ -19,7 +36,7 @@ richness <- timberData %>% select(ends_with(c("richness", "abundance", "cover"))
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Timber Forest Products and Profits"),
+    titlePanel("Non-Timber Forest Products and Profits"),
 
     # Sidebar with a slider input for number of bins 
     fluidRow(
@@ -56,21 +73,39 @@ server <- function(input, output) {
     observeEvent(input$plotBtn,{
       output$profit_richness <- renderPlot({
         
+        
         #ylabel = paste(strsplit(input$richness, "_")[[1]][1], strsplit(input$richness, "_")[[1]][2])
         #xlabel = paste(strsplit(input$profits, "_")[[1]][1], strsplit(input$profits, "_")[[1]][2])
-        isolate({
+        isolate({   #produce plot only on click
+          
+          #find the appropriate image
+          img = pickImage(strsplit(input$richness, " ")[[1]][1])
           
           req(input$profits, input$richness)
           
-          timbdat() %>%
+          plt <- timbdat() %>%
           ggplot(aes(.data[[input$profits]], .data[[input$richness]], col=Habitat)) +
+ 
           geom_point(size=2, alpha=0.7)+
           scale_color_manual(values=c("#003f5c", "#ffa600"))+
           scale_y_continuous(limits = c(0, NA)) + 
           #ylab(ylabel) + 
           #xlab(xlabel) +
           geom_smooth(method=lm , se=F) +
-          theme_bw()
+          #theme_bw() + 
+          theme(panel.background = element_rect(fill='transparent'),
+                #plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
+                panel.grid.major = element_blank(), #remove major gridlines
+                panel.grid.minor = element_blank(),
+                axis.line = element_line(colour = "black"),
+                legend.background = element_rect(fill='transparent'), #transparent legend bg
+                legend.box.background = element_rect(fill='transparent') #transparent legend panel
+          )
+          
+          #add image to plot
+          plt + inset_element(p = img, left = 0.8, bottom = 0.8, right = 1, 
+                          top = 0.95, clip=TRUE, on_top=FALSE) +
+          theme(rect  = element_rect(fill="transparent"))
         }) # end of isolate  
     }, res=120)
     }) #end of button click observeEvent
